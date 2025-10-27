6d24d540986fafafbad918b41ec0459bb1602152
NoobRoblox <goodscripts001@gmail.com>
2025-10-27 16:41:58 +0700

    Create Method.lua

---
+++ Method.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NoobRoblox/Ha/refs/heads/main/W-script"))()
+local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/PhatLee877/ynhi/refs/heads/main/moreiraynhi"))()
