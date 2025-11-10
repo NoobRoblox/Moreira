8a7a9279127a83a4aaf01af6da8d23006acbf989
NoobRoblox <goodscripts001@gmail.com>
2025-11-10 14:27:53 +0700

    Update Method.lua

--- Method.lua
+++ Method.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/8B1AavIznSEVjXkw/raw"))()
-local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/PhatLee877/ynhi/refs/heads/main/moreiraynhi"))()
+local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/NoobRoblox/Sab/refs/heads/main/Noob"))()
